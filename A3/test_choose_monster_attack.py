99fce57f68badaf5dab74408afb80fdb00c1b0ca
clintonbf <cfernandes24@my.bcit.ca>
2019-11-05 22:02:19 -0800

    choose_monster_attack() unit tests tests everything in the list

--- A3/test_choose_monster_attack.py
+++ A3/test_choose_monster_attack.py
@@ -5,8 +5,9 @@ from A3.monster import choose_monster_attack
 
 
 class TestChoose_monster_attack(TestCase):
-    @patch('random.choice', side_effect=['kitty'])
+    @patch('random.choice', side_effect=['kitty', 'puppy', 'otter', 'longsword', 'AK-47', 'thermonuclear device'])
     def test_choose_monster_attack(self, mock_choice):
         attacks = ['kitty', 'puppy', 'otter', 'longsword', 'AK-47', 'thermonuclear device']
 
-        self.assertEqual(choose_monster_attack(attacks), 'kitty')
+        for attack in attacks:
+            self.assertEqual(choose_monster_attack(attacks), attack)
