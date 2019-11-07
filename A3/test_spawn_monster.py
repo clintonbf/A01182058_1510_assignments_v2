7d3da6c576cba2d7d80e8e0eff3939257a95cf08
clintonbf <cfernandes24@my.bcit.ca>
2019-11-07 15:29:04 -0800

    Corrected test_spawn_monster()

--- A3/test_spawn_monster.py
+++ A3/test_spawn_monster.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from A3.monster import spawn_monster
 
 
 class TestSpawn_monster(TestCase):
-    def test_spawn_monster(self):
-        expected_m = {'Name': 'Neda', 'HP': {'Current': 5, 'Max': 5}, 'Dexterity': 0, 'Class': 'monster'}
+    @patch('random.choice', side_effect=['Neda'])
+    def test_spawn_monster(self, mock_choice):
+        expected_m = {'Name': 'Neda', 'HP': {'Current': 5, 'Max': 5}, 'Dexterity': 10, 'Class': 'monster'}
 
         m = spawn_monster()
 
