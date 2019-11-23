1e179da867aa0071aad4ced84a10c35d28edd340
clintonbf <cfernandes24@my.bcit.ca>
2019-11-23 15:58:15 -0800

    Logic complete for question 8. Added tests for find_max_value_in_a_dictionary()

--- A4/question_8.py
+++ A4/question_8.py
@@ -1,19 +1,49 @@
-def find_max_bar_in_a_group(d: dict, l: list) -> int:
+def find_max_value_in_a_dictionary(d: dict, l: list) -> int:
     """
-    Find the number with the highest amount of bars in a group
+    Find the highest value in a sub-section of a dictionary.
+
     :param d: dictionary
     :param l: list
-    :precondition: l has no other values than what is contained in d
-    :postcondition: find the value in l that has the most amount of lines representing it
+    :precondition: l keys must be in d
+    :postcondition: the highest value in the sub-section of d, defined by the keys as specified in l, is determined
 
     :return: int
 
-    >>> find_max_bar_in_a_group({3: 6, 4: 4, 1:2}, [3, 4, 1])
-    3
+    >>> find_max_value_in_a_dictionary({3: 6, 4: 5, 1:2, 9: 18, 12: 2}, [3, 4, 1])
+    6
     """
-    temp_dict = {k: v for k, v in d.items() if k in l}
+    count = 0
+    for k in d:
+        if k in l:
+            count += 1
+            break
 
-    return max(temp_dict.values())
+    if count == 0:
+        raise IndexError("At least one key in d must exist in l")
+
+    temp_list = [v for k, v in d.items() if k in l]
+
+    return max(temp_list)
+
+
+def find_first_key_of_a_value(val: int, d: dict) -> str:
+    """
+    Find the key for the first instance of a value in a dictionary.
+
+    :param val: int, the value to look for
+    :param d: dictionary, the dictionary to look through
+    :precondition: val must exist in d
+    :precondition: values in d must in integers
+    :postcondition: the first dictionary key with the given value in the dictionary is given
+    :return: str
+
+    >>> find_first_key_of_a_value(1, {'apple': 5, 'cherry': 1, 'koala': 1, 'rhubarb': 7})
+    'cherry'
+    """
+
+    for k, v in d.items():
+        if v == val:
+            return k
 
 
 def im_not_sleepy() -> str:
@@ -28,7 +58,7 @@ def im_not_sleepy() -> str:
     """
     max_bars = []
 
-    time_bars = {0: 6, 1: 2, 2: 6, 3: 6, 4: 4, 5: 6, 6: 6, 7: 3, 8: 7, 9: 6}
+    time_bars = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
     digit_possibilities = {'hour_tens': [1], 'hour_ones': [0, 1, 2, 4, 5, 6, 7, 8, 9],
                            'minute_tens': [0, 1, 2, 3, 4, 5], 'minute_ones': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
 
@@ -36,18 +66,33 @@ def im_not_sleepy() -> str:
     max_bars.append(max(time_bars.values()))
 
     # By the same token, the biggest 10's minutes is just whatever is the biggest within its own set
-    max_bars.append(find_max_bar_in_a_group(time_bars, digit_possibilities['minute_tens']))
+    max_bars.insert(0, find_max_value_in_a_dictionary(time_bars, digit_possibilities['minute_tens']))
 
-    # The hours is tricky because we can have a single digit hour OR we can have a 2-digit
-    # The question is: is any valid 2-digit hour more lines than the highest valid single digit?
-    highest_single_digit_hour = find_max_bar_in_a_group(time_bars, digit_possibilities['hour_ones'])
+    # Four hours: is any valid 2-digit hour more lines than the highest valid single digit?
+    single_dgt_hr_bars = find_max_value_in_a_dictionary(time_bars, digit_possibilities['hour_ones'])
 
     # In 2-digit hours, the highest digit in the ten's position is 1 and the highest in the one's position is 2
-    bars_in_a_two_digit_hour = time_bars[digit_possibilities['hour_tens'][0]] + time_bars[2]
+    two_dgt_hr_bars = find_max_value_in_a_dictionary(time_bars, digit_possibilities['hour_tens']) \
+                      + find_max_value_in_a_dictionary(time_bars, [0, 1, 2])
 
-    max_bars.append(bars_in_a_two_digit_hour) if bars_in_a_two_digit_hour > highest_single_digit_hour else max_bars.append(highest_single_digit_hour)
+    if two_dgt_hr_bars > single_dgt_hr_bars:
+        max_bars.insert(0, find_max_value_in_a_dictionary(time_bars, [0, 1, 2]))
+        max_bars.insert(0, find_max_value_in_a_dictionary(time_bars, digit_possibilities['hour_tens']))
+    else:
+        max_bars.insert(0, single_dgt_hr_bars)
 
-    return "The number of bars is " + str(sum(max_bars))
+    bar_sum = sum(max_bars)
+    time = ""
+
+    max_bars.insert(2, ":") if len(max_bars) == 4 else max_bars.insert(1, ':')
+
+    for item in max_bars:
+        if item != ':':
+            time = time + str(find_first_key_of_a_value(item, time_bars))
+        else:
+            time = time + ":"
+
+    return "The time with the highest bars is " + str(time) + " The number of bars is " + str(bar_sum)
 
 
 def main():
