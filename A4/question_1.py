0a8f8d62ec87534644b1a1f56d5ce95246994e1e
clintonbf <cfernandes24@my.bcit.ca>
2019-11-24 08:55:50 -0800

    Removed dead code [elminate_multiples()] from question_1

--- A4/question_1.py
+++ A4/question_1.py
@@ -1,25 +1,6 @@
 import math
 
 
-def eliminate_multiples(lst: list, divisor: int) -> list:
-    """
-    Generate a list of numbers that are not multiples of divisor.
-
-    :param divisor: int
-    :param lst: list
-    :precondition: lst is non-empty
-    :precondition: list contains only integers
-    :precondition: divisor > 0
-    :precondition: the sublist of lst that are not divisible by divisor is created
-    :return: list
-
-    >>> eliminate_multiples([0, 2, 3, 4, 5, 6], 2)
-    [3, 5]
-    """
-
-    return [num for num in lst if num % divisor != 0]
-
-
 def generate_multiples(divisor: int, upperbound: int) -> list:
     """
     Generate a list of multiples of a number.
@@ -57,7 +38,7 @@ def eratosthenes(upperbound: int) -> list:
     []
     """
 
-    # Populate a list to eliminate from. I like this idea because I don't want to iterate through a list and eliminate
+    # Populate a list to eliminate from: I don't want to change list size while iterating through it
 
     if upperbound < 0:
         raise ValueError("upperbound must be greater than 0")
@@ -69,12 +50,10 @@ def eratosthenes(upperbound: int) -> list:
 
     # Range to examine is  [3, upperbound] because all even numbers > 2 are divisible by 2 and thus aren't prime
     for num in range(3, (no_multiples_beyond + 1), 2):
-        if num not in elimination_list:
+        # if num not in elimination_list:
             elimination_list.extend(generate_multiples(num, upperbound))
 
     # Build the final list, excluding any number in elimination_list
     prime_list = [num for num in range(2, (upperbound + 1)) if num not in elimination_list]
 
     return prime_list
-
-    # for i in range(3, (no_multiples_beyond + 1), 2):  # Save this cleverness for later
