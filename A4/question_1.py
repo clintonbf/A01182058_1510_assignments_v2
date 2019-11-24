d78b50d12983850fe787964dc21939418a952b79
clintonbf <cfernandes24@my.bcit.ca>
2019-11-24 08:59:56 -0800

    Removed some unneeded code

--- A4/question_1.py
+++ A4/question_1.py
@@ -48,10 +48,9 @@ def eratosthenes(upperbound: int) -> list:
     # Populate it with all even numbers (other than 2)
     elimination_list = [num for num in range(4, (upperbound + 1), 2)]
 
-    # Range to examine is  [3, upperbound] because all even numbers > 2 are divisible by 2 and thus aren't prime
+    # Examine odd numbers
     for num in range(3, (no_multiples_beyond + 1), 2):
-        # if num not in elimination_list:
-            elimination_list.extend(generate_multiples(num, upperbound))
+        elimination_list.extend(generate_multiples(num, upperbound))
 
     # Build the final list, excluding any number in elimination_list
     prime_list = [num for num in range(2, (upperbound + 1)) if num not in elimination_list]
