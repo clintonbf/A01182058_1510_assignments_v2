b011b759814fd731f63c9b7ec1e9c57283b211d9
clintonbf <cfernandes24@my.bcit.ca>
2019-11-24 23:10:39 -0800

    Think I've got 6 decent unit tests.

--- A4/question_6.py
+++ A4/question_6.py
@@ -1,8 +1,8 @@
 """
-1. intersection count = 0 (parallel)
-2. intersection count = 1
-3. intersection count = infinity (co-incident)
-4. intersection count > 1 <-- I don't think this is possible given only 2 points. If the line is thick it can happen)
-5.
-6.
+1. Test if the lines do not intersect and are parallel eg. ([0, 0], [1, 1]), [(0, 1), (1, 2)]
+2. Test if there is one intersection point eg. ([0, 0], [10, 10]), ([-5, 5], [6, 5])
+3. Test if there are infinite intersection points (co-incident) eg. ([0, 0], [1, 1]), ([0, 0], [1, 1])
+4. Test if lines are points eg. ([0, 0], [0, 0]), ([1, 1], [1, 1])
+5. Test if the lines do not intersect but aren't parallel eg. ([0, 0], [3, 3]) ([-5, 2], [-1, 2])
+6. Test if both lines share one co-ordinate set eg. ([0, 0], [1, 1]), ([0, 0], [0, -5])
 """
