3dc9e232cb1f511362cd233fe8d90b1669c02fde
clintonbf <cfernandes24@my.bcit.ca>
2019-11-24 16:50:52 -0800

    PEP correction

--- A4/test_find_max_value_in_a_dictionary.py
+++ A4/test_find_max_value_in_a_dictionary.py
@@ -5,7 +5,7 @@ from A4.question_8 import find_max_value_in_a_dictionary
 
 class TestFind_max_value_in_a_dictionary(TestCase):
     def test_find_max_value_in_a_dictionary_when_situation_is_simple(self):
-        self.assertEqual(6, find_max_value_in_a_dictionary({3: 6, 4: 5, 1:2, 9: 18, 12: 2}, [3, 4, 1]))
+        self.assertEqual(6, find_max_value_in_a_dictionary({3: 6, 4: 5, 1: 2, 9: 18, 12: 2}, [3, 4, 1]))
 
     def test_find_max_value_in_a_dictionary_if_max_exists_twice(self):
         self.assertEqual(6, find_max_value_in_a_dictionary({3: 6, 4: 5, 1: 6, 9: 18, 12: 2}, [3, 4, 1]))
